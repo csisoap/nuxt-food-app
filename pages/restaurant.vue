22b594ad74e8c41540332080d7d95eecfd5045c8
Jim <hoaianro@gmail.com>
2020-11-13 10:40:59 +0700

    Initial commit

---
+++ pages/restaurant.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Restaurant</h1>
+    <AppRestaurantInfo />
+  </div>
+</template>
+
+<script>
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+
+export default {
+  components: {
+    AppRestaurantInfo
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
