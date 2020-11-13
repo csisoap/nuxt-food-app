51dacfdd726f76041d71066d5c620a2d4940e997
Jim <hoaianro@gmail.com>
2020-11-13 11:34:41 +0700

    Add filter to the restaurant page

--- pages/restaurant.vue
+++ pages/restaurant.vue
@@ -1,16 +1,39 @@
 <template>
-  <div>
-    <h1>Restaurant</h1>
-    <AppRestaurantInfo />
-  </div>
+  <main class="container restaurant">
+    <div class="restaurantheading">
+      <h1>Restaurant</h1>
+      <AppSelect @change="selectedRestaurant = $event" />
+    </div>
+    <AppRestaurantInfo :datasource="filteredRestaurants" />
+  </main>
 </template>
 
 <script>
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+import AppSelect from "@/components/AppSelect.vue";
+import { mapState } from "vuex";
 
 export default {
   components: {
+    AppSelect,
     AppRestaurantInfo
+  },
+  data() {
+    return {
+      selectedRestaurant: ""
+    };
+  },
+  computed: {
+    ...mapState(["fooddata"]),
+    filteredRestaurants() {
+      if (this.selectedRestaurant) {
+        return this.fooddata.filter(el => {
+          let name = el.name.toLowerCase();
+          return name.includes(this.selectedRestaurant);
+        });
+      }
+      return this.fooddata;
+    }
   }
 };
 </script>
